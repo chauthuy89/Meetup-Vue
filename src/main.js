67ca402bee866a9e55430eca93fbf218c408cd92
Rose Le <rose.le.coder@gmail.com>
2020-04-26 16:41:44 +1200

    api

--- src/main.js
+++ src/main.js
@@ -21,13 +21,18 @@ new Vue({
   render: h => h(App),
   created () {
     firebase.initializeApp({
-      apiKey: "AIzaSyBfkB9nOG2QLKOZJCiQVSm82og2A3hiu2I",
-  authDomain: "dev-meetup-vue-621aa.firebaseapp.com",
-  databaseURL: "https://dev-meetup-vue-621aa.firebaseio.com",
-  projectId: "dev-meetup-vue-621aa",
-  storageBucket: "dev-meetup-vue-621aa.appspot.com",
-  messagingSenderId: "503273088886",
-  appId: "1:503273088886:web:3e68ef735a6b734834bc57"
+  //     apiKey: "AIzaSyBfkB9nOG2QLKOZJCiQVSm82og2A3hiu2I",
+  // authDomain: "dev-meetup-vue-621aa.firebaseapp.com",
+  // databaseURL: "https://dev-meetup-vue-621aa.firebaseio.com",
+  // projectId: "dev-meetup-vue-621aa",
+  // storageBucket: "dev-meetup-vue-621aa.appspot.com",
+  // messagingSenderId: "503273088886",
+  // appId: "1:503273088886:web:3e68ef735a6b734834bc57"
+  apiKey: 'AIzaSyCFYWd6FpR53u4hSPXQSjOYeZNPF1FxG2M',
+      authDomain: 'yt-devmeetup.firebaseapp.com',
+      databaseURL: 'https://yt-devmeetup.firebaseio.com',
+      projectId: 'yt-devmeetup',
+      storageBucket: ''
     })
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
